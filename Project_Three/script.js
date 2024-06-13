11cb366578c9ce2939d487176d89c57c3bf3d8d5
SadhwaniMomal <Sadhwanimoomal987@gmail.com>
2024-06-13 06:06:32 -0700

    inital floder

---
+++ Project_Three/script.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("navbar-toggler")
+  .addEventListener("click", function () {
+    const navbar = document.getElementById("main-navbar");
+    navbar.classList.toggle("bg-black");
+  });
